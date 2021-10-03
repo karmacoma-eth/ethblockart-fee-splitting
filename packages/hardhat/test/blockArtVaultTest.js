80834d1e59e7eb2102a4682e741c536b831949b6
karmacoma <karmacoma.eth@gmail.com>
2021-10-03 12:04:19 -0700

    Test that we can update minTreasuryFeeBasisPoints

--- packages/hardhat/test/blockArtVaultTest.js
+++ packages/hardhat/test/blockArtVaultTest.js
@@ -12,10 +12,10 @@ describe("EthBlockArt fee splitting", function () {
   let vault, vaultWithWrongSigner, vaultWithStyleOwner;
   let vaultOwner, styleOwner, someOtherUser, charityUser;
 
-  async function recordBalances() {
-    let coinsBalance = (await vault.coinsBalance()).toNumber();
-    let charityBalance = (await vault.charityBalance()).toNumber();
-    let artistBalance = (await vault.getStyleFees(validStyleId)).toNumber();
+  async function recordBalances(someVault = vault) {
+    let coinsBalance = (await someVault.coinsBalance()).toNumber();
+    let charityBalance = (await someVault.charityBalance()).toNumber();
+    let artistBalance = (await someVault.getStyleFees(validStyleId)).toNumber();
     return [coinsBalance, charityBalance, artistBalance];
   }
 
@@ -42,28 +42,28 @@ describe("EthBlockArt fee splitting", function () {
     describe("when calling depositAndSplit", () => {
       it("should not accept styleFeeBasisPoints = -1", async () => {
         await expect(vault.depositAndSplit(validStyleId, -1, 0)).to.be.reverted;
-      })
+      });
 
       it("should not accept styleFeeBasisPoints > 10000", async () => {
         await expect(vault.depositAndSplit(validStyleId, 123456, 0)).to.be.revertedWith("invalid styleFeeBasisPoints + charityFeeBasisPoints");
-      })
+      });
 
       it("should not accept charityFeeBasisPoints = -1", async () => {
         await expect(vault.depositAndSplit(validStyleId, 0, -1)).to.be.reverted;
-      })
+      });
 
       it("should not accept charityFeeBasisPoints > 10000", async () => {
         await expect(vault.depositAndSplit(validStyleId, 0, 10001)).to.be.revertedWith("invalid styleFeeBasisPoints + charityFeeBasisPoints");
-      })
+      });
 
       it("should not accept styleFeeBasisPoints + charityFeeBasisPoints too big for minTreasuryFeeBasisPoints", async () => {
         await expect(vault.depositAndSplit(validStyleId, 5000, 5000)).to.be.revertedWith("invalid styleFeeBasisPoints + charityFeeBasisPoints");
         await expect(vault.depositAndSplit(validStyleId, 4800, 4800)).to.be.revertedWith("invalid styleFeeBasisPoints + charityFeeBasisPoints");
-      })
+      });
 
       it("should not accept calls with no value", async () => {
         await expect(vault.depositAndSplit(validStyleId, 0, 0)).to.be.revertedWith("msg.value must not be 0");
-      })
+      });
 
       it("should support sending everything to the treasury", async () => {
         let [coinsBalanceBefore, charityBalanceBefore, artistBalanceBefore] = await recordBalances();
@@ -73,7 +73,7 @@ describe("EthBlockArt fee splitting", function () {
         expect(coinsBalanceAfter).to.equal(coinsBalanceBefore + 1000);
         expect(charityBalanceAfter).to.equal(charityBalanceBefore);
         expect(artistBalanceAfter).to.equal(artistBalanceBefore);
-      })
+      });
 
       it("should support sending everything to the artist (minus minTreasuryFeeBasisPoints)", async () => {
         let [coinsBalanceBefore, charityBalanceBefore, artistBalanceBefore] = await recordBalances();
@@ -83,7 +83,7 @@ describe("EthBlockArt fee splitting", function () {
         expect(coinsBalanceAfter).to.equal(coinsBalanceBefore + 50); // 5% to the treasury
         expect(charityBalanceAfter).to.equal(charityBalanceBefore);
         expect(artistBalanceAfter).to.equal(artistBalanceBefore + 950);
-      })
+      });
 
       it("should support sending everything to charity (minus minTreasuryFeeBasisPoints)", async () => {
         let [coinsBalanceBefore, charityBalanceBefore, artistBalanceBefore] = await recordBalances();
@@ -93,7 +93,7 @@ describe("EthBlockArt fee splitting", function () {
         expect(coinsBalanceAfter).to.equal(coinsBalanceBefore + 50); // 5% to the treasury
         expect(charityBalanceAfter).to.equal(charityBalanceBefore + 950);
         expect(artistBalanceAfter).to.equal(artistBalanceBefore);
-      })
+      });
 
       it("should support arbitrary splits like 40% each to charity and artist, 20% to treasury", async () => {
         let [coinsBalanceBefore, charityBalanceBefore, artistBalanceBefore] = await recordBalances();
@@ -103,7 +103,7 @@ describe("EthBlockArt fee splitting", function () {
         expect(coinsBalanceAfter).to.equal(coinsBalanceBefore + 200);
         expect(charityBalanceAfter).to.equal(charityBalanceBefore + 400);
         expect(artistBalanceAfter).to.equal(artistBalanceBefore + 400);
-      })
+      });
 
       it("should support arbitrary splits when the value can't be nicely split", async () => {
         let [coinsBalanceBefore, charityBalanceBefore, artistBalanceBefore] = await recordBalances();
@@ -114,7 +114,7 @@ describe("EthBlockArt fee splitting", function () {
         expect(coinsBalanceAfter).to.equal(coinsBalanceBefore + 201);
         expect(charityBalanceAfter).to.equal(charityBalanceBefore + 400);
         expect(artistBalanceAfter).to.equal(artistBalanceBefore + 400);
-      })
+      });
     });
 
     describe("when calling collectCoins()", () => {
@@ -135,6 +135,7 @@ describe("EthBlockArt fee splitting", function () {
         expect(artistBalanceAfter).to.equal(artistBalanceBefore);
       });
     });
+
 
     describe("when calling collectStyleFees(uint256)", () => {
       it("should not accept withdrawals from random addresses", async () => {
@@ -170,7 +171,7 @@ describe("EthBlockArt fee splitting", function () {
       });
 
       it("should reject transfers to address zero", async () => {
-        await expect(vault.donateCharityBalance(ethers.constants.AddressZero)).to.be.reverted;
+        await expect(vault.donateCharityBalance(ethers.constants.AddressZero)).to.be.revertedWith("beneficiary must not be the zero address");
       });
 
       it("should transfer the correct amount to the designated address", async () => {
@@ -183,6 +184,50 @@ describe("EthBlockArt fee splitting", function () {
         expect(coinsBalanceAfter).to.equal(coinsBalanceBefore);
         expect(charityBalanceAfter).to.equal(0);
         expect(artistBalanceAfter).to.equal(artistBalanceBefore);
+      });
+    });
+  });
+
+  it("should support updating minTreasuryFeeBasisPoints", async () => {
+    let vaultWithNewMinTreasuryFee = await BlockArtVaultFactory.deploy(blockStyle.address);
+    let vaultWithNewMinTreasuryFeeWrongSigner = vaultWithNewMinTreasuryFee.connect(someOtherUser);
+
+    describe("when calling setMinTreasuryFeeBasisPoints(uint256)", () => {
+      it("should reject calls from random addresses", async () => {
+        await expect(vaultWithNewMinTreasuryFeeWrongSigner.setMinTreasuryFeeBasisPoints(0)).to.be.revertedWith("Ownable: caller is not the owner");
+      });
+
+      it("should validate the new value", async () => {
+        await expect(vaultWithNewMinTreasuryFee.setMinTreasuryFeeBasisPoints(123456)).to.be.revertedWith("invalid minTreasuryFeeBasisPoints");
+      });
+
+      it("should support setting minTreasuryFeeBasisPoints to 0%", async () => {
+        await vaultWithNewMinTreasuryFee.setMinTreasuryFeeBasisPoints(0);
+
+        let [coinsBalanceBefore, charityBalanceBefore, artistBalanceBefore] = await recordBalances(vaultWithNewMinTreasuryFee);
+        await vaultWithNewMinTreasuryFee.depositAndSplit(validStyleId, 50 * 100, 50 * 100, {value: 1000});
+        let [coinsBalanceAfter, charityBalanceAfter, artistBalanceAfter] = await recordBalances(vaultWithNewMinTreasuryFee);
+
+        // nothing sent to treasury
+        expect(coinsBalanceAfter).to.equal(coinsBalanceBefore);
+        expect(charityBalanceAfter).to.equal(charityBalanceBefore + 500);
+        expect(artistBalanceAfter).to.equal(artistBalanceBefore + 500);
+      });
+
+      it("should support setting minTreasuryFeeBasisPoints to 100%", async () => {
+        await vaultWithNewMinTreasuryFee.setMinTreasuryFeeBasisPoints(100 * 100);
+
+        let [coinsBalanceBefore, charityBalanceBefore, artistBalanceBefore] = await recordBalances(vaultWithNewMinTreasuryFee);
+        await vaultWithNewMinTreasuryFee.depositAndSplit(validStyleId, 0, 0, {value: 1000});
+        let [coinsBalanceAfter, charityBalanceAfter, artistBalanceAfter] = await recordBalances(vaultWithNewMinTreasuryFee);
+
+        // everything sent to treasury
+        expect(coinsBalanceAfter).to.equal(coinsBalanceBefore + 1000);
+        expect(charityBalanceAfter).to.equal(charityBalanceBefore);
+        expect(artistBalanceAfter).to.equal(artistBalanceBefore);
+
+        // can't event ask for a 0.01% fee split in this case
+        await expect(vaultWithNewMinTreasuryFee.depositAndSplit(validStyleId, 1, 1, {value: 1000})).to.be.revertedWith("invalid styleFeeBasisPoints + charityFeeBasisPoints");
       });
     });
   });
